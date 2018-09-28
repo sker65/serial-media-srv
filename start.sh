e272dd1b6ef3a88376cc3b50c4961920f2dcee5a
Stefan Rinke <sker65@gmail.com>
2018-09-28 22:37:44 +0200

    - start script with update mechanism - service definition for systemd auto start

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# update handling
+if [ -f /boot/update/update.zip ]; then
+    mv /boot/update/update.zip /boot/update/update-done.zip
+    unzip -o /boot/update/update-done.zip -d /home/pi/serial-media-srv
+    sync
+fi
+
+# log rotate
+mv /boot/data/serial.log.1 /boot/data/serial.log.2
+mv /boot/data/serial.log /boot/data/serial.log.1
+
+cd /boot/data
+python3 /home/pi/serial-media-srv/img-mov-serial.py  &> /boot/data/serial.log
