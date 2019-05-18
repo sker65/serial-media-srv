7e7eec692816b6342ff1ec040d56855958afdfb1
Stefan Rinke <sker65@gmail.com>
2019-05-18 18:41:35 +0200

    - completely reworked version 1.0.4 with new commands - better exception handling in service loop

--- img-mov-serial.py
+++ img-mov-serial.py
@@ -26,12 +26,24 @@ import threading
 from shutil import copyfile
 import configparser
 import logging
+from time import sleep
 
 from pathlib import Path
 #from omxplayer.player import OMXPlayer
 
 config = configparser.ConfigParser()
+# defaults
+config['general']={}
+config['general']['device']='/dev/ttyS0'
+config['general']['baud'] = '57600'
+config['general']['timeout'] = '100000'
 config['player']={}
+config['player']['exec'] = 'omxplayer -b'
+config['viewer']={}
+config['viewer']['exec'] = 'fim -a -q'
+config['slide']={}
+config['slide']['exec'] = "fim -a -q -c 'while(1){display;sleep\"5\";next;}'"
+
 config.read('serial-media-srv.ini')
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
@@ -43,13 +55,13 @@ if newStart.is_file():
     copyfile("./new-start.sh", "./start.sh")
     newStart.unlink()
 
-version = '1.0.3'
+version = '1.0.4'
 
 basedir = '.'
 if len(sys.argv) > 1:
     basedir = sys.argv[1]
 
-serial_device = '/dev/ttyS0'
+serial_device = config['general']['device']
 if len(sys.argv) > 2:
     serial_device = sys.argv[2]
 
@@ -60,12 +72,19 @@ def signal_handler(sig, frame):
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
+readFromFile = ''
 
 log.info( "serial-media-srv version {} starting ...".format(version ))
+if config.has_option('general', 'readFrom'):
+    readFromFile = config['general']['readFrom']
 
-port = serial.Serial(serial_device, baudrate=57600, timeout=1000000)
+baud=int(config['general']['baud'])
 
-log.info( "serial port {} initilized".format(serial_device))
+if len(readFromFile)==0:
+    port = serial.Serial(serial_device, baudrate=baud, timeout=int(config['general']['timeout']))
+    log.info( "serial port {} initilized with {}".format(serial_device, baud))
+else:
+    file = open( readFromFile, "r")
 
 rx = re.compile( r'[0-9]+\.(jpg|png)$' )
 images = {}
@@ -110,7 +129,7 @@ class Task(object):
         self.playing = id
 
 def onProcessExit(task):
-    global processes
+    global processes, follow_task
     log.info("player process {} playing {} exited".format(task.proc.pid, task.playing))
     del processes[task.proc.pid]
     if len(processes) == 0 and follow_task == 0: check_for_defaults()
@@ -148,52 +167,104 @@ def term_running():
             task.proc.send_signal(signal.SIGINT)
             task.proc.kill()
 
-def play_movie(id, m):
+def play_movie(name):
     global movie_playing, config, log
-    movie_playing = id
-    player = popenAndCall(onProcessExit, 1, id, ['omxplayer', '-b', Path(m[id]).absolute().as_posix() ], stdin=PIPE, stdout=PIPE, shell=False )
+    movie_playing = name
+    args = config['player']['exec'].split()
+    args.append( Path(name).absolute().as_posix() )
+    log.debug("running: {}".format(args))
+    player = popenAndCall(onProcessExit, 1, name, args, stdin=PIPE, stdout=PIPE, shell=False )
     #player = Popen(['omxplayer', '-b', Path(m[id]).absolute().as_posix() ], stdin=PIPE, stdout=PIPE, shell=False  )
-    log.debug( "playing movie {} in process {}".format(m[id],player.pid))
+    log.debug( "playing movie {} in process {}".format(name,player.pid))
     #player.set_aspect_mode(config['player'].get('aspectMode','stretch'))
     #player.stopEvent += lambda _: movie_ended()
 
-def show_image(id, m):
+def show_image(name):
     global log
-    log.debug( "showing image {}".format(m[id]))
-    fim = popenAndCall(onFimProcessExit, 2, id, ['fim', '-a', '-q', m[id] ], stdin=PIPE, stdout=PIPE, shell = False)
-    #fim = Popen(['fbi', '-a', '-d', '/dev/fb0', '-noverbose', m[id] ], stdin=PIPE, stdout=PIPE, shell = False)
-    #outs, errs = fim.communicate(input="j".encode('utf-8'),timeout=1)
+    log.debug( "showing image '{}'".format(name))
+    args = config['viewer']['exec'].split()
+    args.append( Path(name).absolute().as_posix() )
+    log.debug("running: {}".format(args))
+    fim = popenAndCall(onFimProcessExit, 2, name, args, stdin=PIPE, stdout=PIPE, shell = False)
     log.debug( "started fbi process {}".format(fim.pid))
 
+def run_slideshow(path_pattern):
+    global log
+    log.debug("running slideshow '{}'".format(path_pattern))
+    args = config['slide']['exec'].split()
+    args.append( path_pattern )
+    log.debug("running: {}".format(args))
+    fim = popenAndCall(onFimProcessExit, 2, name, args, stdin=PIPE, stdout=PIPE, shell = False)
+    log.debug( "playing slide {} in process {}".format(name,player.pid))
+
 def check_for_defaults():
-    global defaultmovies, defaultimages
+    global defaultmovies, defaultimages, movie_playing
     if len(defaultmovies) > 0:
         id = random.randint(0,len(defaultmovies)-1)
-        play_movie(id, defaultmovies)
+        play_movie(defaultmovies[id])
     if len(defaultimages) > 0:
         id = random.randint(0,len(defaultimages)-1)
-        show_image(id, defaultimages)
+        movie_playing = None
+        show_image(defaultimages[id])
 
+def handleCmd( tokens ):
+    global follow_task, log
+    cmd = tokens[0]
+    log.debug("handling cmd '{}' ntok {}".format(cmd,len(tokens)))
+    if cmd == 'STOP':
+        term_running()
+        # not necessary on exit handler does it //check_for_defaults()
+    elif cmd == 'PLAY' and len(tokens) > 1:
+        media = tokens[1]
+        if media.endswith('.png') or media.endswith('.jpg'):
+            term_running()
+            show_image(media)
+        else: 
+            if movie_playing != media:
+                follow_task = 1
+                term_running()
+                play_movie(media)
+            else:
+                log.info("same movie already playing ({})".format(movie_playing))
+    elif cmd == 'SLEEP' and len(tokens) > 1:
+        sleep( int(tokens[1]) )
+    elif cmd == 'SLIDE' and len(tokens) > 1:
+        term_running()
+        run_slideshow( tokens[1] )
+    elif cmd == 'PLAYONEOF' and len(tokens) > 1:
+        media = tokens[1:]
+        # choose one
+        i = random.randint(0,len(media)-1)
+        movie = media[i]
+        if movie != movie_playing:
+            follow_task = 1
+            term_running()
+            play_movie(movie)
+        else:
+            log.info("same movie already playing ({})".format(movie_playing))
+
+
+def readNextLine():
+    global readFromFile, file
+    if not readFromFile:
+        r = port.readline().strip().decode('ISO-8859-1')
+        log.debug("got line '{}'".format(r))
+        return r
+    else:
+        r = file.readline()
+        return r
 
 # main loop
 check_for_defaults()
 while True:
     follow_task = 0
-    device = port.read()
-    event =  port.read()
-    if len(device)==0 or len(event)==0:
+    line = readNextLine()
+    if len(line) == 0:
         continue
-    id = ord(device) * 256 + ord(event)
-    log.debug( "Event received: {}:{} = {}". format(device,event,id) )
-    #exit()
-    if id == 0:
-        term_running()
-        check_for_defaults()
+    tokens = line.split()
+    if len(tokens) == 0:
         continue
-    if id in images:
-        term_running()
-        show_image(id, images)
-    if id in movies and movie_playing != id:
-        follow_task = 1
-        term_running()
-        play_movie(id, movies)
+    try:
+        handleCmd( tokens )
+    except:
+        continue    
