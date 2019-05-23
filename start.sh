82e76761375cccd89d8efba991e38fef4a7a79a1
Stefan Rinke <sker65@gmail.com>
2019-05-23 12:00:15 +0200

    - usb stick support

--- start.sh
+++ start.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
+
+# usb stick support: detect if this path exists, if so use it as base path for update, soundset logging
+BASEPATH=/boot
+if [ -d /media/usb0/data ]; then
+	BASEPATH=/media/usb0
+fi
+if [ -d /media/usb1/data ]; then
+	BASEPATH=/media/usb1
+fi
+if [ -b /dev/sda ]; then
+	BASEPATH=/media/usb
+fi
+
+# copy default jpg if missing
+if [ ! -f $BASEPATH/data/default.jpg ]; then
+    cp /boot/data/default.jpg $BASEPATH/data/
+fi
+
 # update handling
-if [ -f /boot/update/update.zip ]; then
-    mv /boot/update/update.zip /boot/update/update-done.zip
-    unzip -o /boot/update/update-done.zip -d /home/pi/serial-media-srv
+if [ -f $BASEPATH/update/update.zip ]; then
+    mv $BASEPATH/update/update.zip $BASEPATH/update/update-done.zip
+    unzip -o $BASEPATH/update/update-done.zip -d /home/pi/serial-media-srv
     sync
 fi
 
 # log rotate
-mv /boot/data/serial.log.1 /boot/data/serial.log.2
-mv /boot/data/serial.log /boot/data/serial.log.1
+mv $BASEPATH/data/serial1.log $BASEPATH/data/serial2.log
+mv $BASEPATH/data/serial.log $BASEPATH/data/serial1.log
 
-cd /boot/data
-python3 /home/pi/serial-media-srv/img-mov-serial.py  >> /boot/data/serial.log 2>&1
+cd $BASEPATH/data
+python3 /home/pi/serial-media-srv/img-mov-serial.py  >> $BASEPATH/data/serial.log 2>&1
